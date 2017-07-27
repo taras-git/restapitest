23dfe17d0f25462c95f8190f565a6445e18d7c41
Taras Tymchyshyn <TTymchyshyn@luxoft.com>
2017-07-27 14:05:53 +0200

    Add csv utils

--- src/test/java/PropertyFinderTester.java
+++ src/test/java/PropertyFinderTester.java
@@ -5,8 +5,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,9 +16,10 @@ import java.util.stream.Collectors;
 import static com.jayway.restassured.RestAssured.given;
 import static junit.framework.TestCase.assertTrue;
 
+import utils.CSVUtils;
+
 
 public class PropertyFinderTester {
-
 
     @Test
     public void testPropertyFinderIsUp() {
@@ -31,10 +34,10 @@ public class PropertyFinderTester {
 
 
     @Test
-    public void testPropertyFiderLeastExpensive() throws IOException {
+    public void testPropertyFinderLeastExpensive() throws IOException {
 
-        List<Appartament> appartamentList = new ArrayList<>();
-        List<Appartament> appartamentListFiltered;
+        List<Apartment> appartamentList = new ArrayList<>();
+        List<Apartment> appartamentListFiltered;
 
         // get html code of 1st page of property finder site
         Document page = Jsoup.connect("https://www.propertyfinder.ae/search?l=&q=&c=2&t=&rp=y&pf=&pt=&bf=&bt=&af=&at=&fu=0&kw=").get();
@@ -64,7 +67,7 @@ public class PropertyFinderTester {
                             .replace(",", ""));
 
             // create new appartment object
-            Appartament app = new Appartament();
+            Apartment app = new Apartment();
             app.setBedrooms(bedroomsNum);
             app.setPrice(price);
             app.setAddress(address);
@@ -80,17 +83,17 @@ public class PropertyFinderTester {
         appartamentListFiltered = appartamentList.stream()
                 .filter(addr -> addr.getAddress().contains("Marina"))
                 .filter(bdr -> bdr.getBedrooms() >= 2)
-                .sorted(Comparator.comparing(Appartament::getPrice))
+                .sorted(Comparator.comparing(Apartment::getPrice))
                 .collect(Collectors.toList());
 
         // print the results
-        for (Appartament a : appartamentListFiltered) {
+        for (Apartment a : appartamentListFiltered) {
             System.out.println(a.getAddress());
             System.out.println(a.getPrice());
         }
 
         // check if the last element of the filtered list has at least 2 bedrooms
-        Appartament last = appartamentListFiltered.stream()
+        Apartment last = appartamentListFiltered.stream()
                 .reduce((a, b) -> b)
                 .orElse(null);
 
@@ -99,11 +102,101 @@ public class PropertyFinderTester {
         System.out.println("LAST appartment has bedrooms: " + last.getBedrooms());
     }
 
+    @Test
+    public void testPropertyFinderVilla3Beds7Beds() throws IOException {
 
-    class Appartament {
+        List<Apartment> apartmentList = new ArrayList<>();
+        List<Apartment> appartmentListFiltered;
+
+        String villaToBuy = "https://www.propertyfinder.qa/search?l=&q=&c=1&t=35&pf=&pt=&bf=&bt=&af=&at=&kw=";
+        String appInfoCssLocator = ".listing-content";
+
+        // get html code of 1st htmlCodePage of property finder site
+        Document htmlCodePage = Jsoup.connect(villaToBuy).get();
+        // get appartments info from the 1st htmlCodePage
+        Elements elementsAppInfo = htmlCodePage.select(appInfoCssLocator);
+
+        // get appartments info from some next pages
+        for (int i = 2; i < 4; i++) {
+            htmlCodePage = Jsoup.connect(villaToBuy + "&htmlCodePage=" + i).get();
+            Elements newAppInfo = htmlCodePage.select(appInfoCssLocator);
+            for (Element info : newAppInfo) {
+                elementsAppInfo.add(info);
+            }
+        }
+
+        String addressCssLocator        = "div.location-tree";
+        String bedroomsCssLocator       = "div.property-details > span:nth-child(2)";
+        String priceCssLocator          = "span.price > span.val";
+        String currencyCssLocator       = ".currency";
+
+        // iterate over results to crate proper Appartment objects
+        for (Element e : elementsAppInfo) {
+            // extract address
+            String address = e.select(addressCssLocator).text();
+            // extract number of bedrooms
+            String bedrooms = e.select(bedroomsCssLocator).text().replaceAll("\\D+", "");
+            // convert number of bedrooms to integer
+            Integer bedroomsNum = Integer.parseInt(bedrooms);
+            // extract price for the appartment
+            Integer price = Integer.parseInt(
+                    e.select(priceCssLocator)
+                            .text()
+                            .replace(",", ""));
+            // get currency
+            String currency = e.select(currencyCssLocator).text();
+
+            // create new appartment object
+            Apartment app = new Apartment();
+            app.setBedrooms(bedroomsNum);
+            app.setPrice(price);
+            app.setAddress(address);
+            app.setCurrency(currency);
+
+            // populate list with the new appartment
+            apartmentList.add(app);
+        }
+
+        // filter list of appartment for: address has word "The Pearl"
+        // filter resulted list of appartment for: bedrooms minimum 3 and maximum 7
+        // sort the resulted list by price
+        // populate appartmentListFiltered list with the results
+        appartmentListFiltered = apartmentList.stream()
+                .filter(addr -> addr.getAddress().contains("The Pearl"))
+                .filter(bdr -> (bdr.getBedrooms() >= 3) && (bdr.getBedrooms() <= 7))
+                .sorted(Comparator.comparing(Apartment::getPrice))
+                .collect(Collectors.toList());
+
+        // generate csv file
+        String csvFile = "villa.csv";
+        FileWriter writer = new FileWriter(csvFile);
+        // print the results and generate csv file
+        for (Apartment a : appartmentListFiltered) {
+            System.out.println(a.getAddress());
+            System.out.println(a.getPrice());
+            System.out.println(a.getCurrency());
+            CSVUtils.writeLine(writer, Arrays.asList(a.getAddress(),
+                    a.getPrice().toString() + " " + a.getCurrency()), '-');
+        }
+        writer.flush();
+        writer.close();
+    }
+
+
+    class Apartment {
         private Integer bedrooms;
         private Integer price;
         private String address;
+
+        public String getCurrency() {
+            return currency;
+        }
+
+        public void setCurrency(String currency) {
+            this.currency = currency;
+        }
+
+        private String currency;
 
         public void setAddress(String address) {
             this.address = address;
